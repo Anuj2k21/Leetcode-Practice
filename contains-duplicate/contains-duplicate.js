d6431b0238f8baef8e7cbf42309257a6044f423d
Anuj kashyap <95622350+Anuj2k21@users.noreply.github.com>
2022-07-27 22:10:06 +0530

    Time: 126 ms, Memory: 51.8 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,16 @@
+
+var containsDuplicate = function(nums) {
+  var appears = {};
+  var containsDuplicate = false;
+  for (var i = 0; i < nums.length; i++) {
+    if (appears[nums[i]]) {
+      appears[nums[i]] += 1;
+      containsDuplicate = true;
+      break;
+    } else {
+      appears[nums[i]] = 1;
+    }
+  }
+
+  return containsDuplicate;
+};
