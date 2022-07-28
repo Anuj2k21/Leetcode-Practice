dd3f24c6ccbc39bcb076aec847881ff5d5f08dbd
Anuj kashyap <95622350+Anuj2k21@users.noreply.github.com>
2022-07-29 01:49:11 +0530

    Time: 88 ms (89.29%), Space: 42.1 MB (97.58%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+    
+    var isAnagram = function(S, T) {
+    let len = S.length, fMap = new Int16Array(123)
+    if (T.length !== len) return false
+    for (let i = 0; i < len; i++)
+        fMap[S.charCodeAt(i)]++
+    for (let i = 0; i < len; i++)
+        if (--fMap[T.charCodeAt(i)] < 0) return false
+    return true
+};
+    
