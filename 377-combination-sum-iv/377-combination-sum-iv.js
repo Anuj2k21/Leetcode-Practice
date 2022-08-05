c440d1cdab14f82960dc9110ff6d451fe01c2381
Anuj kashyap <95622350+Anuj2k21@users.noreply.github.com>
2022-08-05 23:15:24 +0530

    Time: 69 ms (88.43%), Space: 41.9 MB (94.22%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.js
@@ -0,0 +1,15 @@
+var combinationSum4 = function(nums, target) {
+    // Create dp array
+    const dp_arr = Array(target+1).fill(0)
+    // Set default
+    dp_arr[0] = 1
+    // Loop until we hit target
+    for(let i = 0; i <= target;i++) {
+        // Loop through all possible nums
+        for(let j = 0; j < nums.length; j++) {
+            // If the sum of the current position in dp and the current num is less than target, increment the index at the sum in dp array by all the ways to make dp[i]
+            if(nums[j]+i <= target) dp_arr[nums[j]+i] += dp_arr[i]
+        }
+    }
+    return dp_arr[target]
+};
